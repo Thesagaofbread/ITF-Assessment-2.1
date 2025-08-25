c8e19ede3f461032604930eefb382e5abe7e0caa
John Doe <mangoandthecrew@gmail.com>
2025-08-25 12:21:58 +1000

    testing stage 2- did not upload all
    # went though the code, filing system, etc

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -5,3 +5,50 @@ Dynamic content updates (e.g., product gallery, customer reviews).
 
 Simple user interactions (e.g., navigation menu, image slider).*/
 
+
+//Functions for page display 
+
+//Content display - consistent across all pages - prevent loading when not needed
+totalPages = document.querySelectorAll(".display")
+var activePage = null
+
+for(let i = 0; i < totalPages.length; i++){
+
+
+    if(totalPages[i] != homepage){
+
+        totalPages[i].setAttribute("hide", "true");
+
+    }else{
+
+        totalPages[i].setAttribute("hide", "false");
+        activePage = totalPages[i]
+
+    };
+
+};
+function changePage(pageid){
+
+    if(pageid != activePage){
+
+        pageid.setAttribute("hide", "false");
+        activePage.setAttribute("hide", "true");
+        activePage = pageid;
+
+    };
+    
+;}
+
+//Image slider
+
+//Style functions- media queries reaction,etc
+
+//Style function 1- Image/product update(portfolio item)
+
+//Style function 2- navbar
+
+//Style function 3 - dropdowns
+
+//Form response/store- contact and book - read and compile
+
+//cache clearing and loading content
